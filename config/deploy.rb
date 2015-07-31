c348159f1915e9d91fb67a475550572ea3fdaf30
kunal <kunalb8607@gmail.com>
2015-07-31 14:10:18 +0530

    update script

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid only for current version of Capistrano
+
+
+set :stages, %w(production staging)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
+set :application, "myapp"
+set :repository, 'git@github.com:kunal-webonise/url_shortner.git'
+set :scm, :git
+set :use_sudo, false
+set :deploy_to, "/home/ubuntu/www/url_shortner"
+
+task :do_bundle_install do
+
+  run  'cd /home/ubuntu/www/demo_app/current && bundle install && rake db:create && rake db:migrate'
+  run 'echo restarting apache'
+  run 'sudo /etc/init.d/apache2 restart'
+end
+
+after 'deploy', 'do_bundle_install'
